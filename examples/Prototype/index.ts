75ec1bce11a120fa3e3b4171dede57d998802a7d
Kirill Gavrilov <vseriousv@MacBook-Pro-Kirill.local>
2020-01-21 17:36:22 +0700

    Checked prototype pattern

--- examples/Prototype/index.ts
+++ examples/Prototype/index.ts
@@ -1,42 +1,42 @@
 class Prototype {
     public primitive: any;
     public component: object;
-    public circularReferance: ComponentWithBackReference;
-
+    public referLink: LinkReferBack;
+    
     public clone(): this {
         const clone = Object.create(this);
 
         clone.component = Object.create(this.component);
 
-        clone.circularReferance = {
-            ...this.circularReferance,
-            prototype: { ...this },
+        clone.referLink = {
+            ...this.referLink,
+            prototype: {...this}
         }
 
         return clone;
     }
 }
 
-class ComponentWithBackReference {
+class LinkReferBack {
     public prototype;
 
-    constructor(prototype: Prototype) {
+    constructor(prototype: Prototype){
         this.prototype = prototype;
     }
 }
 
 function clientCode(){
     const p1 = new Prototype();
-    p1.primitive = 245;
-    p1.component = new Date();
-    p1.circularReferance = new ComponentWithBackReference(p1);
+    p1.primitive = 765;
+    p1.component = new Date;
+    p1.referLink = new LinkReferBack(p1);
 
     const p2 = p1.clone();
-
-    console.log("p1.primitive:", p1.primitive, "p2.primitive:", p2.primitive, "p1.primitive === p2.primitive:", p1.primitive === p2.primitive);
-    console.log("p1.primitive:", p1.component, "p2.primitive:", p2.component, "p1.primitive === p2.primitive:", p1.component === p2.component);
-    console.log("p1.circularReferance:", p1.circularReferance, "p2.circularReferance:", p2.circularReferance, "p1.circularReferance === p2.circularReferance:", p1.circularReferance === p2.circularReferance);
-    console.log("p1.circularReferance.prototype:", p1.circularReferance.prototype, "p2.circularReferance.prototype:", p2.circularReferance.prototype, "p1.circularReferance.prototype === p2.circularReferance.prototype:", p1.circularReferance.prototype === p2.circularReferance.prototype);
+    console.log("p1: ", p1);
+    console.log("p2: ", p2);
+    console.log("p2.__proto__: ", Object.getPrototypeOf(p2));
+    console.log("p1.prim: ", p1.primitive);
+    console.log("p2.prim: ", p2.primitive);
 }
 
 clientCode();
